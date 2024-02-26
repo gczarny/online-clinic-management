a68fa6532967aef9ebf42490852422060c4d51d0
gczarny <grzegorz.czarny93@gmail.com>
2024-02-26 18:02:45 +0100

    initial commit

---
+++ src/main/java/pl/online_clinic_management/OnlineClinicManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.online_clinic_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineClinicManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineClinicManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/online_clinic_management/OnlineClinicManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.online_clinic_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClinicManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
